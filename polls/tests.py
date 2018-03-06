8f17a591c8b8402544078a6844628795636e01a2
superlll <superl@udel.edu>
2018-03-06 11:27:13 -0500

    Initial commit

---
+++ polls/tests.py
@@ -0,0 +1,68 @@
+from django.urls import reverse
+import datetime
+from django.utils import timezone
+from django.test import TestCase
+from .models import Question
+
+def create_question(question_text, days):
+    """
+    创建一个以 question_text 为标题，pub_date 为 days 天之后的问题。
+    days 为正表示将来，为负表示过去。
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        如果数据库里没有保存问题，应该显示一个合适的提示信息。
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        """
+        值 pub_date 是过去的，问题应该被显示在主页上。
+        """
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_future_question(self):
+        """
+        值 pub_date 是将来的，问题不应该被显示在主页上。
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        如果数据库里同时存有过去和将来的投票，那么只应该显示过去那些。
+        """
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_two_past_questions(self):
+        """
+        问题索引页应该可以显示多个问题。
+        """
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+        )
